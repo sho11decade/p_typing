844f8bfec2834a2497c32d17b0d98bcdeaada19e
sauce21 <sho11decade@gmail.com>
2024-04-28 22:25:39 +0900

    英語タイピングの関連ファイルのパスを更新

--- programming.js
+++ programming.js
@@ -1,14 +1,3 @@
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-	navigator.serviceWorker.register('sw.js').then(registration => {
-	console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	}, err => {
-	console.log('ServiceWorker registration failed: ', err);
-	}).catch(err => {
-	console.log(err)
-	});
-	});
-}
 let Q = ["apple", "banana", "melon", "mango", "starwberry", "blueberry", "orange","rasberry","abcdefg"];//問題文
 let Q_No = Math.floor(Math.random() * Q.length);//問題をランダムで出題する
 
